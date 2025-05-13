3fdd68e2e4dbab7dfdb0ac00ac1caab4c09fd9c6
suleymanrhemel <suleymandogan213@gmail.com>
2025-05-13 19:09:56 +0300

    Add project files.

---
+++ Sube1.EmlakciLib/KiralikEv.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sube1.EmlakciLib
+{
+    public class KiralikEv : Ev
+    {
+        public KiralikEv()
+        {
+
+        }
+
+        public KiralikEv(double kira, double depozito, int odasayisi, string semt, int katno, double alan):base(odasayisi,katno,alan,semt)
+        {
+            this.Kira = kira;
+            this.Depozito = depozito;           
+        }
+
+        public double Kira { get; set; }
+        public double Depozito { get; set; }
+
+     
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}\nKira:{this.Kira}\nDepozito:{this.Depozito}";
+        }
+    }
+}
+
+
